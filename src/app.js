a1e478adeb28e32c408c161451b5bcac6d7f1ea1
akioo <=>
2017-10-25 17:56:32 -0200

    Initial commit again

--- src/app.js
+++ src/app.js
@@ -1,15 +1,57 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const VoteKickController = require("./controller/VoteKickController.js");
 
+//control vote kick
+let isVoteKickInProgress = false;
+const VOTE_KICK_LABEL = "!votekick";
+
+let voteKickController;
 client.on("ready", () => {
-    console.log("I am ready!");
+    voteKickController = new VoteKickController(client);
 });
+function a(){
+    let command = {};
+    command.content = "!votekick JNymiD Chen";
+    voteKick(command);
+    console.log(VoteKick);
+}
 
-client.on("message", (message) => {
-    if (message.content.startsWith("ping")) {
-        message.channel.send("pong!");
+
+function voteKick(message){
+    let username = "";
+    if (message.content.startsWith(VOTE_KICK_LABEL)) {
+        username = message.content.substr(
+            message.content.indexOf(VOTE_KICK_LABEL) + VOTE_KICK_LABEL.length,
+            message.content.length
+        ).trim();
+
+        let user = getUser(username, client.users);
+        console.log(user);
+        return (user);
     }
-});
+}
+
+function getUser(username, users){
+    let toKickUser = null;
+    users.forEach(user => {
+        if(user.username === username && !user.bot){
+            toKickUser = user;
+        };
+    });
+    return toKickUser;
+}
+
+function isVoteKick(message, users){
+    if(isVoteKick){
+        // Already a vote kick in progress
+        return false;
+    }
+
+    if(message.content.startsWith("!votekick")){
+
+    }
+}
 
 //APP_BOT_USER TOKEN
-client.login("TOKEN");
+client.login("");
